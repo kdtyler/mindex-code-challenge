c1c958385f719b3d50d938f96dd61687f300a690
mindex <dev@mindex.com>
2024-12-06 18:58:19 -0600

    Basic implementation of Task 1. Still need to check full graph possibilities and Unit Tests

--- src/main/java/com/mindex/challenge/data/Employee.java
+++ src/main/java/com/mindex/challenge/data/Employee.java
@@ -1,8 +1,14 @@
 package com.mindex.challenge.data;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+
 import java.util.List;
 
 public class Employee {
+//    @Id
+//    private String id;
+//    @Indexed(unique = true)
     private String employeeId;
     private String firstName;
     private String lastName;
--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -2,11 +2,14 @@ package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -40,10 +43,63 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
+
     @Override
     public Employee update(Employee employee) {
         LOG.debug("Updating employee [{}]", employee);
 
         return employeeRepository.save(employee);
     }
+
+    @Override
+    public ReportingStructure getReportingStructure(String employeeId) {
+        LOG.debug("Getting reporting structure for employee with id [{}]", employeeId);
+        Employee employee = read(employeeId);
+        int numberOfReports = calculateNumberOfReports(employee);
+        return new ReportingStructure(employee, numberOfReports);
+    }
+
+    private int calculateNumberOfReports(Employee employee) {
+        List<Employee> directReports = employee.getDirectReports();
+        int count = 0;
+        if (directReports != null) {
+            count += directReports.size();
+            for (Employee directReport : directReports) {
+                count += calculateNumberOfReports(directReport);
+            }
+        }
+        return count;
+    }
+
+    /*
+    @Override
+    public Employee update(Employee employee) {
+    LOG.debug("Updating employee [{}]", employee);
+
+    Employee existingEmployee = employeeRepository.findByEmployeeId(employee.getEmployeeId());
+    if (existingEmployee == null) {
+        throw new RuntimeException("Employee not found with id: " + employee.getEmployeeId());
+    }
+
+    if (employee.getFirstName() != null) {
+        existingEmployee.setFirstName(employee.getFirstName());
+    }
+    if (employee.getLastName() != null) {
+        existingEmployee.setLastName(employee.getLastName());
+    }
+    if (employee.getPosition() != null) {
+        existingEmployee.setPosition(employee.getPosition());
+    }
+    if (employee.getDepartment() != null) {
+        existingEmployee.setDepartment(employee.getDepartment());
+    }
+    if (employee.getDirectReports() != null) {
+        existingEmployee.setDirectReports(employee.getDirectReports());
+    }
+
+
+    return employeeRepository.save(existingEmployee);
+    }
+
+     */
 }
